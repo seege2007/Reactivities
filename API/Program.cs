369a3dae6e34b62a4bfaeb15e0f48c6711d3c8c1
Christopher Jones <chrsmjones@gmail.com>
2022-09-09 17:18:40 -0500

    Inital Project Setup

--- API/Program.cs
+++ API/Program.cs
@@ -1,46 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Persistence;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace API
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-          var host = CreateHostBuilder(args).Build();
-
-          using var scope = host.Services.CreateScope();
-
-          var services = scope.ServiceProvider;
-
-          try
-          {
-              var context = services.GetRequiredService<DataContext>();
-              await context.Database.MigrateAsync();
-              await Seed.SeedData(context);
-          }
-          catch (Exception ex)
-          {
-              var logger = services.GetRequiredService<ILogger<Program>>();
-              logger.LogError(ex, "An error occured during migration");
-          }
-
-          await host.RunAsync();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
